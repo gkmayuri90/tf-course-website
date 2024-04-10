e8967fa1fb919853bf3dce95c62ba483c48dd21b
gkmayuri90 <gkmayuri90@gmail.com>
2024-04-10 08:55:51 +0000

    Initial Commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.2.0"
+    }
+  }
+ backend "gcs" {
+    bucket  = "roidtc-240408-user529-tf-state"
+    prefix  = "terraform/state"
+  }
+
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
